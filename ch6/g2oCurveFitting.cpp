c7fb4a382835538dcabe041e0d760f3eec2d9936
xujiabin <xujiabin@jtrobots.com>
2022-07-31 09:11:42 +0800

    all

--- ch6/g2oCurveFitting.cpp
+++ ch6/g2oCurveFitting.cpp
@@ -15,6 +15,54 @@
 using namespace std;
 
 // 曲线模型的顶点，模板参数：优化变量维度和数据类型
+//g2o::BaseVertex<D,T> D是int 类型的，表示vertex的最小维度，比如3D空间中旋转是3维的，那么这里 D = 3
+                                              //T是待估计vertex的数据类型，比如用四元数表达三维旋转的话，T就是Quaternion 类型 
+/*
+VertexSE2 : public BaseVertex<3, SE2>  //2D pose Vertex, (x,y,theta)
+VertexSE3 : public BaseVertex<6, Isometry3>  //6d vector (x,y,z,qx,qy,qz) (note that we leave out the w part of the quaternion)
+VertexPointXY : public BaseVertex<2, Vector2>
+VertexPointXYZ : public BaseVertex<3, Vector3>
+VertexSBAPointXYZ : public BaseVertex<3, Vector3>
+
+// SE3 Vertex parameterized internally with a transformation matrix and externally with its exponential map
+VertexSE3Expmap : public BaseVertex<6, SE3Quat>
+
+// SBACam Vertex, (x,y,z,qw,qx,qy,qz),(x,y,z,qx,qy,qz) (note that we leave out the w part of the quaternion.
+// qw is assumed to be positive, otherwise there is an ambiguity in qx,qy,qz as a rotation
+VertexCam : public BaseVertex<6, SBACam>
+
+// Sim3 Vertex, (x,y,z,qw,qx,qy,qz),7d vector,(x,y,z,qx,qy,qz) (note that we leave out the w part of the quaternion.
+VertexSim3Expmap : public BaseVertex<7, Sim3>   //VertexSE3Expmap  李代数表示位姿
+*/
+
+//! 重新定义顶点一般需要考虑重写如下函数：
+// virtual bool read(std::istream& is);
+// virtual bool write(std::ostream& os) const;  //read，write：分别是读盘、存盘函数，一般情况下不需要进行读/写操作的话，仅仅声明一下就可以
+// virtual void oplusImpl(const number_t* update);  //顶点更新函数。非常重要的一个函数，主要用于优化过程中增量△x 的计算。
+                                                                                                            //我们根据增量方程计算出增量之后，就是通过这个函数对估计值进行调整的，因此这个函数的内容一定要重视
+// virtual void setToOriginImpl();     //顶点重置函数，设定被优化变量的原始值
+//自己定义 顶点一般是下面的格式：
+
+// class myVertex: public g2o::BaseVertex<Dim, Type>
+//   {
+//       public:
+//       EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+//       myVertex(){}
+
+//       virtual void read(std::istream& is) {}
+//       virtual void write(std::ostream& os) const {}
+
+//       virtual void setOriginImpl()
+//       {
+//           _estimate = Type();
+//       }
+//       virtual void oplusImpl(const double* update) override
+//       {
+//           _estimate += /*update*/;
+//       }
+//   }
+
 class CurveFittingVertex : public g2o::BaseVertex<3, Eigen::Vector3d> 
 {
 public:
@@ -23,13 +71,14 @@ public:
   // 重置
   virtual void setToOriginImpl() override
   {
-    _estimate << 0, 0, 0;
+      // _estimate = Eigen::Vector3d(0, 0, 0);
+      _estimate << 0, 0, 0;    //定点是三维的，初始值设为0
   }
 
   // 更新
   virtual void oplusImpl(const double *update) override
   {
-    _estimate += Eigen::Vector3d(update);
+    _estimate += Eigen::Vector3d(update);   //初始值更新，因为是向量，所以可以直接加上去。
   }
 
   // 存盘和读盘：留空
@@ -37,6 +86,19 @@ public:
 
   virtual bool write(ostream &out) const {}
 };
+
+//边
+//BaseUnaryEdge，BaseBinaryEdge，BaseMultiEdge 分别表示一元边，两元边，多元边
+
+
+
+
+
+
+
+
+
+
 
 
 // 误差模型 模板参数：观测值维度，类型，连接顶点类型
@@ -48,7 +110,7 @@ public:
   CurveFittingEdge(double x) : BaseUnaryEdge(), _x(x) {}
 
   // 计算曲线模型误差
-  virtual void computeError() override 
+  virtual void computeError() override    //是使用当前顶点的值计算的测量值与真实的测量值之间的误差
   {
     const CurveFittingVertex *v = static_cast<const CurveFittingVertex *> (_vertices[0]);
     const Eigen::Vector3d abc = v->estimate();
@@ -101,18 +163,25 @@ int main(int argc, char **argv)
     y_data.push_back(exp(ar * x * x + br * x + cr) + rng.gaussian(w_sigma * w_sigma));
   }
 
-  // 构建图优化，先设定g2o
+// 构建图优化，先设定g2o
+//   3是维度，更改维度在这里,  其中p代表pose的维度（注意一定是流形manifold下的最小表示），l表示landmark的维度，
+//   BlockSolver_6_3 ：表示pose 是6维，观测点是3维。用于3D SLAM中的BA  BlockSolver_6_3 = BlockSolver< BlockSolverTraits<p, l> >
+//   BlockSolver_7_3：在BlockSolver_6_3 的基础上多了一个scale                       BlockSolver_7_3 = BlockSolver< BlockSolverTraits<p, l> >
+//   BlockSolver_3_2：表示pose 是3维，观测点是2维                                                 BlockSolver_3_2 = BlockSolver< BlockSolverTraits<p, l> >
   typedef g2o::BlockSolver<g2o::BlockSolverTraits<3, 1>> BlockSolverType;  // 每个误差项优化变量维度为3，误差值维度为1
-  
+
   //LinearSolverDense ：使用dense cholesky分解法。继承自LinearSolver
   typedef g2o::LinearSolverDense<BlockSolverType::PoseMatrixType> LinearSolverType; // 线性求解器类型
 
   // 梯度下降方法，可以从GN, LM, DogLeg 中选
   //这里是高斯牛顿
+//  g2o::OptimizationAlgorithmGaussNewton
+// g2o::OptimizationAlgorithmLevenberg 
+// g2o::OptimizationAlgorithmDogleg 
   auto solver = new g2o::OptimizationAlgorithmGaussNewton( g2o::make_unique<BlockSolverType>(g2o::make_unique<LinearSolverType>()));
   g2o::SparseOptimizer optimizer;     // 图模型
   optimizer.setAlgorithm(solver);   // 设置求解器
-  optimizer.setVerbose(true);       // 打开调试输出
+  optimizer.setVerbose(true);       // 打开调试输出, 其中setVerbose是设置优化过程输出信息用的
 
 /*
 
